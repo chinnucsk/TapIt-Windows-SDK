caba974e6229effcad36ba68e259077baa4289a7
raginikulkarni <raginik@cybage.com>
2013-09-10 19:39:32 +0530

    conditional compilation for WP8 and Win8

---
+++ TapIt-Win8/TapIt-Win8/ResourceStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TapIt_Win8
+{
+  public static class ResourceStrings
+    {
+      public static string AdControlName = "TapItAdViewControl";
+      public static string BaseUrl = "http://dev.tapit.com/~npenteado/adrequest.php";
+      public static string BaseUrl_Local = "http://ec2-107-20-3-62.compute-1.amazonaws.com/~chetanch/adrequest.php";
+      public static string ErrorResponse = "Response Contains Error";
+      public static string FileName = "13A91515-EA40-4E1C-A322-09347B84DCED.txt";
+      public static string Format = "json";
+      public static string LoadNewAd = "Please Load New Ad";
+      public static string NavigationErrorMsg = "Failed To Navigate";
+      public static string NoAdPromptData = "No Data Available for AdPrompt";
+      public static string SdkVersion = "wp-v1.0.0";
+      public static string SuccessMesg = "Success";
+      public static string TrackerMesg = "Install Is Already Tracked";
+      public static string TrackHandler = "/adconvert.php";
+      public static string TrackHost = "a.tapit.com";
+    }
+}
