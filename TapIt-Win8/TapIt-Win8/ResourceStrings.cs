499ee2b227365895554403ec56913a826ece0e78
raginikulkarni <raginik@cybage.com>
2013-09-16 19:53:08 +0530

    added tracker

--- TapIt-Win8/TapIt-Win8/ResourceStrings.cs
+++ TapIt-Win8/TapIt-Win8/ResourceStrings.cs
@@ -12,15 +12,19 @@ namespace TapIt_Win8
         public static string BaseUrl = "http://dev.tapit.com/~npenteado/adrequest.php";
         public static string BaseUrl_Local = "http://ec2-107-20-3-62.compute-1.amazonaws.com/~chetanch/adrequest.php";
         public static string ErrorResponse = "Response Contains Error";
-        public static string FileName = "13A91515-EA40-4E1C-A322-09347B84DCED.txt";
+        public static string FileName = "13A91515-EA40-4E1C-A322-09347B84DCED.tapit";
         public static string Format = "json";
         public static string LoadNewAd = "Please Load New Ad";
         public static string NavigationErrorMsg = "Failed To Navigate";
         public static string NoAdPromptData = "No Data Available for AdPrompt";
-        public static string SdkVersion = "wp-v1.0.0";
+        public static string SdkVersion = "win-v1.0.0";
         public static string SuccessMesg = "Success";
         public static string TrackerMesg = "Install Is Already Tracked";
         public static string TrackHandler = "/adconvert.php";
         public static string TrackHost = "a.tapit.com";
+        public static string Location_Disabled = "Location services are disabled";
+        public static string Location_Service_Unavailable = "Location services are not available";
+        public static string Location_Data_Unavailable = "Location data is not available";
+        public static string Location_Failed = "Failed to get location data";
     }
 }
